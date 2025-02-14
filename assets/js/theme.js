938bcd3cd8205132004ae751fac66b83799c1008
Sadık Orak <muhammedsadikorak@gmail.com>
2025-02-15 02:19:34 +0300

    modal için html kodlandı css hem dark hem de light için kodlandı. theme.js te başlangış olarak tarayıcının tema rengi seçildi.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,13 @@
 const toogleButton = document.querySelector('#theme-btn');
 const body = document.body;
 
+const userPreferance = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+body.setAttribute('data-theme', userPreferance);
+
 toogleButton.addEventListener('click', (e) => {
   const theme = e.target.checked ? "dark" : "light";
   body.setAttribute('data-theme', theme);
-
-  console.log(theme);
   
 });
 
