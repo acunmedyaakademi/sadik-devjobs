9d90fd024bbb83e9f55e503556c46374f9223745
Sadık Orak <muhammedsadikorak@gmail.com>
2025-02-17 09:57:23 +0300

    data dışarıdan fetch ile alındı ve dinamik olarak ekrana basıldı. Card lara basınca detay sayfasına cardId yönlendirildi.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,68 @@
-const dialog = document.querySelector('.mabile-dialog');
+const filePath = "assets/json/app.json";
+let jobList = [];
 
-const filterDialog = document.querySelector('.fa-filter');
+const contents = document.querySelector('.contents');
 
-filterDialog.addEventListener('click', (e) => {
 
-  dialog.showModal();
 
-  dialog.addEventListener('click', (e) => {
-    if (e.target === dialog) {
-      dialog.close();
-      console.log(true);
+
+
+
+
+async function getData() {
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error("verilere erişilemiyor");
     }
-  })
+
+    const data = await response.json();
+    jobList = data;
+
+    renderMain();
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
+function renderMain() {
+  contents.innerHTML = "";
 
-});
+
+  jobList.forEach(job => {
+    contents.innerHTML +=
+      `
+      <a href="#" class="card" data-id="${job.id}">
+        <img src="${job.logo}" alt="">
+        <div class="timeline">
+          <span>${job.postedAt}</span>
+          <span>.</span>
+          <span>${job.contract}</span>
+        </div>
+        <h3>${job.position}</h3>
+        <p class="card-company">${job.company}</p>
+        <p class="card-country">${job.location}</p>
+      </a>
+    `
+  });
+
+  bindElement();
+}
+
+function bindElement() {
+
+  const cards = document.querySelectorAll('.card');
+  console.log(cards);
+
+  cards.forEach(card => card.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const jobId = e.target.closest('a').dataset.id
+    window.location.href = `/detail.html?jobId=${jobId}`;
+  }))
+}
+
+
+getData();
